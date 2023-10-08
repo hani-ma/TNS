9885557204d917ccba51dca5f7f3715a67f732d1
Umme Hani M A <143514834+hani-ma@users.noreply.github.com>
2023-10-08 23:03:20 +0530

    Update MyClass.java

--- Staticmethod/MyClass.java
+++ Staticmethod/MyClass.java
@@ -1,5 +1,3 @@
-package org.cec.staticconcept.staticblockmethod;
-
 public class MyClass {
 	private int section ; //non-static or instance variable
 	private static int srNo; //static or class variable
